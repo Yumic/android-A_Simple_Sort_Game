91ea5aedc29b0b386951cc618e6ddbac7a2f5e98
Yumic <lightning201@qq.com>
2015-09-26 23:05:20 +0800

    add Android Project

---
+++ app/src/main/java/com/example/oathkeeper/android_a_simple_sort_game/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.oathkeeper.android_a_simple_sort_game;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.button_open_album)
+    Button buttonOpenAlbum;
+    @Bind(R.id.imageView_picture)
+    ImageView imageViewPicture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        buttonOpenAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);//ACTION_OPEN_DOCUMENT
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            Log.d("uri", uri.toString());
+            ContentResolver contentResolver = this.getContentResolver();
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(uri));
+                ImageView imageView = (ImageView) findViewById(R.id.imageView_picture);
+                //将Bitmap设定到ImageView
+                imageView.setImageBitmap(bitmap);
+                //由于4.4以上和以下获取的uri不同，因此区别对待
+                if(android.os.Build.VERSION.SDK_INT>=android.os.Build.VERSION_CODES.KITKAT){
+                    Log.d("test", "realPath 4.4以上------>" + GetPathUtils.getPathAfterKitkat(this, uri));
+                }else{
+                    Log.d("test", "realPath 4.4以下------>" + GetPathUtils.getPathBeforeKitkat(this,uri));
+                }
+            } catch (FileNotFoundException e) {
+                Log.e("Exception", e.getMessage(), e);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+
+
+
+
+
+}
